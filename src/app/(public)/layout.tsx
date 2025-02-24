4053048041e05a4cff5dece3d63f8b67b0dc523b
Claudio Freda <me@froggyc.eu>
2025-02-24 01:34:08 +0100

    moved pagetitle below

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -9,7 +9,6 @@ export default async function PublicLayout({
 }>) {
   return (
     <>
-      <PageTitle title="Prenota una sessione" />
       <p>
         Alla FroggyCon, ciascuna sessione inizia da uno scenario, preparato o generato al momento: un punto d&apos;inizio, senza fine premeditata.
         Vi siederete a un tavolo con gli altri partecipanti e il facilitatore. Ascoltatelo e aiutatelo quando potete: il suo ruolo è di insegnarvi a giocare.
@@ -18,6 +17,7 @@ export default async function PublicLayout({
         pronti a scoprire un&apos;esperienza unica e originale che nessuno di voi potrà prevedere.
         Giocherete per circa tre ore, alla fine delle quali il gioco si interromperà a qualunque punto esso sia arrivato: non è garantito un finale, perché per noi <strong>il viaggio conta più della meta</strong>, e soprattutto che quel viaggio si svolga senza forzature.
       </p>
+      <PageTitle title="Prenota una sessione" />
       {children}
     </>
   );
