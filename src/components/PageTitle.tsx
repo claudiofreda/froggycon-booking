5311b304df5a334b46d27355fae43136f72baa4d
Claudio Freda <me@froggyc.eu>
2025-02-24 01:53:44 +0100

    more margin

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -1,3 +1,3 @@
 export const PageTitle = ({ title }: { title: string }) => {
-  return <h1 className="mb-2 mt-3 font-bold text-xl">{title}</h1>;
+  return <h1 className="mb-2 mt-4 font-bold text-xl">{title}</h1>;
 };
